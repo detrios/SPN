4e341c05273a4d82d32908ef11f70727cf07adaa
detrios <developpeurntic@gmail.com>
2014-02-11 10:56:25 +0100

    toto

---
+++ main.js
@@ -0,0 +1,33 @@
+function onDeviceReady() {
+    navigator.splashscreen.hide();
+}
+document.addEventListener("deviceready", onDeviceReady, false);
+
+
+
+$(document).ready(function (){
+
+window.plugins.videoPlayer.play("http://www.youtube.com/watch?v=E0UV5i5jY50");
+
+/*$.ajax({
+   type: 'GET',
+    url: 'http://www.starpirates.fr/API/API.php',
+    jsonpCallback: 'API_SC2',
+    contentType: "application/json",
+    dataType: 'jsonp',
+    data:'action=ship',
+    async:true,
+    success: function(data) {
+       $('#ship').html('');
+		for(var i =0; i< data.ship.total; i++){
+			$('#ship').append('<img width="90" onclick="location.href=this.src" src="https://robertsspaceindustries.com/rsi/static/images/game/ship-specs/'+data.ship[i].imageurl+'" style="float:left" />'+data.ship[i].manufacturer+' : '+data.ship[i].title+'<br style="clear:both" />');
+		}
+    },
+    error: function(e) {
+       console.log(e.message);
+    }
+});*/
+
+});
+
+
